ef6fb3393b135fc7f10db19e1a6143c3cd7b0207
NuFe <fontanaedera@yahoo.com>
2021-11-17 16:11:50 +0100

    templateTic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TIC-TAC-TOE</h1>
+      <div className='table'>
+        <div className='item'>O</div>
+        <div className='item'></div>
+        <div className='item'>X</div>
+        <div className='item'>X</div>
+        <div className='item'>1</div>
+        <div className='item'>1</div>
+        <div className='item'>1</div>
+        <div className='item'>1</div>
+        <div className='item'>1</div>
+      </div>
     </div>
   );
 }
